d1f4010c6309e595e575f90141afa9a0078154d9
timtan93 <timtan93@gmail.com>
2018-12-12 21:44:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences #http://charliepark.tumblr.com/post/1063697980/splitting-strings-in-ruby-when-you-have-multiple
+    self.split(/[?!.] /).size
   end
+
 end
